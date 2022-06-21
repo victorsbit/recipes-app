69873ca4dc0ec93bbcb39c81ad3961fc3b49769d
Nick <66397994+Nicksam97@users.noreply.github.com>
2022-06-21 16:55:31 -0300

    Feat Profile
    Acrescentando Requisitos da pagina de Profile.
    Reqs 82 a 87

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import './Profile.css';
 
 function Profile() {
+  const history = useHistory();
+  const email = JSON.parse(localStorage.getItem('user'));
+
   return (
-    <div>
+    <div className="profile-all">
       <Header title="Profile" showBt={ false } />
-      <h1>Conteúdo</h1>
+      <h2 data-testid="profile-email">{ `User: ${email.email}` }</h2>
+
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => {
+          localStorage.clear();
+          history.push('/');
+        } }
+      >
+        Logout
+      </button>
+
       <Footer />
     </div>
   );
