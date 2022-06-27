df0f5285af2bdfec58723153932cad3d53d23370
Nick <66397994+NickMendes@users.noreply.github.com>
2022-06-27 14:44:07 -0300

    lgin

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -39,6 +39,8 @@ function Login() {
       <fieldset className="login-form">
         <legend>Login</legend>
 
+        <h1>APP de Receitas</h1>
+
         <label htmlFor="email">
           Email:
           <input
